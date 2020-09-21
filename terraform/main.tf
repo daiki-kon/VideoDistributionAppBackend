e22403b32c9774253134e0e08de82665e80ddd8b
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2020-09-21 23:42:37 +0900

    add public access block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "video_repo" {
   bucket = "video-distribution-video-repo"
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "video_repo" {
+  bucket                  = aws_s3_bucket.video_repo.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
