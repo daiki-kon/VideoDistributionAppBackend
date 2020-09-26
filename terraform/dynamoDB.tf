decf84b5579d5febe6b4c06d3e81345cc243c8df
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2020-09-26 22:19:11 +0900

    add lambda layer janome

--- terraform/dynamoDB.tf
+++ terraform/dynamoDB.tf
@@ -54,6 +54,18 @@ data "archive_file" "invert_index_ddb_stream" {
   output_path = "terraform/scripts/invertIndex.zip"
 }
 
+data "archive_file" "janome_lambda_layer" {
+    type        = "zip"
+    source_dir = "modules/janome/"
+    output_path = "modules/janome.zip"
+}
+
+resource "aws_lambda_layer_version" "lambda_layer" {
+    filename = data.archive_file.janome_lambda_layer.output_path
+    layer_name = "janome"
+    compatible_runtimes = ["python3.8"]
+}
+
 resource "aws_lambda_function" "invert_index_ddb_stream" {
   function_name = "invert_index_ddb_stream"
   handler       = "invertIndex.lambda_handler"
