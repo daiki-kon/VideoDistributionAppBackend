fb3b6bbb2563ce43fa35c18852e08fb979c1ff5c
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2020-09-26 16:29:09 +0900

    add dynamoDB stream

---
+++ terraform/cognito.tf
@@ -0,0 +1,118 @@
+resource "aws_cognito_user_pool" "user_manage" {
+  name = "VideoDistributionApp"
+
+  password_policy {
+    minimum_length                   = 6
+    require_lowercase                = false
+    require_numbers                  = false
+    require_symbols                  = false
+    require_uppercase                = false
+    temporary_password_validity_days = 7
+  }
+}
+
+resource "aws_cognito_user_pool_client" "user_manage" {
+  name = "VideoDistributionAppClient"
+
+  user_pool_id = aws_cognito_user_pool.user_manage.id
+}
+
+resource "aws_cognito_identity_pool" "user_manage" {
+  identity_pool_name               = "VideoDistributionApp"
+  allow_unauthenticated_identities = false
+
+  cognito_identity_providers {
+    client_id     = aws_cognito_user_pool_client.user_manage.id
+    provider_name = aws_cognito_user_pool.user_manage.endpoint
+  }
+
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "user_manage" {
+  identity_pool_id = aws_cognito_identity_pool.user_manage.id
+
+  roles = {
+    "authenticated"   = aws_iam_role.authenticated.arn
+    "unauthenticated" = aws_iam_role.unauthenticated.arn
+  }
+}
+
+resource "aws_iam_role" "authenticated" {
+  name = "video_distribution_app_client_cognito_authenticated"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "${aws_cognito_identity_pool.user_manage.id}"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "authenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "authenticated" {
+  name = "video_distribution_app_client_cognito_authenticated_policy"
+  role = aws_iam_role.authenticated.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "unauthenticated" {
+  name = "video_distribution_app_client_cognito_unauthenticated"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "${aws_cognito_identity_pool.user_manage.id}"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "unauthenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool_client.user_manage.id
+}
---
+++ terraform/dynamoDB.tf
@@ -0,0 +1,118 @@
+resource "aws_dynamodb_table" "video_info" {
+  name           = "VideoDistributionVideoInfo"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "VideoId"
+
+  attribute {
+    name = "VideoId"
+    type = "S"
+  }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+
+  tags = {
+    App = "VideoDistribution"
+  }
+}
+
+resource "aws_dynamodb_table" "inverted_index" {
+  name           = "VideoDistributionInvertedIndex"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "Key"
+  range_key      = "VideoId"
+
+  attribute {
+    name = "Key"
+    type = "S"
+  }
+
+  attribute {
+    name = "VideoId"
+    type = "S"
+  }
+
+  tags = {
+    App = "VideoDistribution"
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "invert_index" {
+  event_source_arn  = aws_dynamodb_table.video_info.stream_arn
+  function_name     = aws_lambda_function.invert_index_ddb_stream.arn
+  starting_position = "LATEST"
+}
+
+# Lambda
+data "archive_file" "invert_index_ddb_stream" {
+  type        = "zip"
+  source_dir  = "scripts/invertIndex/"
+  output_path = "scripts/invertIndex.zip"
+}
+
+resource "aws_lambda_function" "invert_index_ddb_stream" {
+  function_name = "invert_index_ddb_stream"
+  handler       = "invertIndex.lambda_handler"
+  role          = aws_iam_role.invert_index_ddb_stream.arn
+  runtime       = "python3.8"
+
+  filename         = data.archive_file.invert_index_ddb_stream.output_path
+  source_code_hash = data.archive_file.invert_index_ddb_stream.output_base64sha256
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.invert_index_ddb_stream.function_name}"
+  retention_in_days = 3
+}
+
+resource "aws_iam_role" "invert_index_ddb_stream" {
+  name = "invert_index_ddb_stream_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "invert_index_ddb_stream_attatch_LambdaBasicExecution" {
+  role       = aws_iam_role.invert_index_ddb_stream.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+// DynamoDB Stream の読み込み権限
+resource "aws_iam_role_policy" "invert_index_ddb_stream_allow_stream" {
+  role   = aws_iam_role.invert_index_ddb_stream.id
+  name   = "invert_index_ddb_stream_"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "dynamodb:DescribeStream",
+                "dynamodb:GetRecords",
+                "dynamodb:GetShardIterator",
+                "dynamodb:ListStreams"
+            ],
+            "Resource": "${aws_dynamodb_table.video_info.stream_arn}",
+            "Effect": "Allow"
+        }
+    ]
+}
+EOF
+}
