4b3d9a105a2bf152576afadf908537f5e16253d7
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2020-10-10 20:58:34 +0900

    add serch lambda code β

--- terraform/scripts/search_video/search_video.py
+++ terraform/scripts/search_video/search_video.py
@@ -1,34 +1,37 @@
 import json
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
+from decimal import Decimal
+from boto3.dynamodb.types import TypeDeserializer
 import os
 
 INVERTED_TABLE_NAME   = os.environ['INVERTED_TABLE_NAME']
 VIDEO_INFO_TABLE_NAME = os.environ['VIDEO_INFO_TABLE_NAME']
 
+# NOTE: ページネーションについては考えられていない
+
+# NOTE: 戻り地がURLエンコードされているので
+#       フロント側でデコードする必要がある
+
 def lambda_handler(event: dict, context):
-    
+
     search_string_list = event['queryStringParameters']['searchString'].split()
-    
-    print(search_string_list)
-    
+
     dynamodb = boto3.resource('dynamodb')
-    
+
     invertedTable = dynamodb.Table(INVERTED_TABLE_NAME)
-    
+
     video_ids = set()
-    
+
     for word in search_string_list:
         res = invertedTable.query(
             KeyConditionExpression=Key('Key').eq(word)
         )
-        print(res)
         if 0 != len(res['Items']):
-        
-            print(res['Items'][0]['VideoIds'])
+
             video_ids = video_ids.union(res['Items'][0]['VideoIds'])
 
-    print(video_ids)
+    print('video_ids: ',video_ids)
     
     
     video_info_table = dynamodb.Table(VIDEO_INFO_TABLE_NAME)
@@ -36,16 +39,32 @@ def lambda_handler(event: dict, context):
     video_info_list = []
     for id in video_ids:
         res = video_info_table.query(
-            KeyConditionExpression=Key('VideoId').eq(id)
+            KeyConditionExpression=Key('videoId').eq(id)
         )
-        print(res)
 
+        if 0 != len(res['Items']):
+
+            print(res['Items'][0])
+            
+            video_info_list.append(res['Items'][0])
+
+    print(video_info_list)
     
+    ret_dict = {
+        "videos":video_info_list
+    }
     return {
           'statusCode': 200,
           'headers': {
               'Content-Type': 'application/json',
               'Access-Control-Allow-Origin': '*' 
           },
-          'body': json.dumps(event)
+          'body': json.dumps(ret_dict, default = default)
       }
+      
+def default(obj):
+    if isinstance(obj, Decimal):
+        if int(obj) == obj:
+            return int(obj)
+        else:
+            return float(obj) 
